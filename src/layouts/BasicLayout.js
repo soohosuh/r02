0e64786434d4099edfb6d743d9f6f5dfd4cc5670
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-07-06 18:30:46 +0900

    230706 리액트 라우팅

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -2,14 +2,12 @@ import SampleNav from "./nav/SampleNav";
 
 const BasicLayout = ({children}) => {
     return ( 
-        <div className="container mx-auto bg-gray-700 h-[100vh]">
+        <div className="container mx-auto bg-gray-700 min-w-[1280px]">
             <div>
                 <SampleNav></SampleNav>
             </div>
             <div>
-                <img src={require('../image/spzl1.jpg')} width={300}/>
-                <img src={require('../image/spzl2.jpg')} width={300}/>
-                <img src={require('../image/spzl3.jpg')} width={300}/>
+                {children}
             </div>
         </div>
     );
--- src/routers/root.js
+++ src/routers/root.js
@@ -2,6 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import AboutPage from "../pages/AboutPage";
 import ListPage from "../pages/board/ListPage";
+import IndexPage from "../pages/board/IndexPage";
+
+import { Suspense, lazy } from "react";
+import LoadingPage from "../pages/LoadingPage";
+
+const Loading = <LoadingPage></LoadingPage>
+const Board_Index = lazy(() => import("../pages/board/IndexPage"))
+const Board_List = lazy(() => import("../pages/board/ListPage"))
 
 //라우터 설정
 //https://reactrouter.com/en/main/start/tutorial 참조
@@ -16,11 +24,11 @@ const router = createBrowserRouter([
     },
     {
         path: "board",
-        element: <div>Board</div>,
+        element: <Suspense fallback={Loading}><Board_Index/></Suspense>,
         children: [
           {
             path: "list",
-            element: <ListPage></ListPage>
+            element: <Suspense fallback={Loading}><Board_List/></Suspense>
           }
         ]
     }
